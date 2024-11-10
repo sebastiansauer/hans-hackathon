ec7839a8e6d14c6374df38f500936827573251f4
Sebastian Sauer <ssauer@posteo.de>
2024-11-10 12:40:15 +0100

    rebuild docs

--- funs/rm_sensitive_data.R
+++ funs/rm_sensitive_data.R
@@ -1,15 +1,11 @@
+library(tidyverse)
 
-rm_sensitive_data <- function(input_path = "data-raw/matomo_export_2024-05-27_to_2024-06-03",
-                              filename, 
-                              output_path = "data-processed/data-raw-no-sensitive") {
-  
-  d <- read.csv(paste0(input_path, "/", filename))
-  d["visitIp"] <- NULL
-  d["visitorId"] <- NULL
-  d["fingerprint"] <- NULL
-  
-  write.csv(d, paste0(output_path, "/", filename))
-  
-  cat("File has been written.")
-  
-}
+
+source("funs/rm_sensitive_data.R")
+
+datafiles_list <- list.files(path = "data-raw/matomo_export_2024-05-27_to_2024-06-03",
+                             pattern = "csv$")
+
+
+data_files_list |> 
+  walk(~ rm_sensitive_data(filename = .x))
