fa46c23a972cb10f3157f6c4c8653a7a651d7374
Sebastian Sauer <ssauer@posteo.de>
2025-01-06 22:10:41 +0100

    sol 08

--- funs/diff_time.R
+++ funs/diff_time.R
@@ -1,7 +1,7 @@
 diff_time <- function(data, group = id_visit, time = time) {
   data |> 
     filter(type ==  "timestamp") |> 
-    group_by(group) |> 
+    group_by({{ group }}) |> 
     mutate(type = as.character(type)) |> 
     mutate(time = parse_date_time(value, "ymd HMS"),
             time_diff = max(time) - min(time)) 
--- run-pipeline.R
+++ run-pipeline.R
@@ -9,19 +9,12 @@ tar_make()
 
 # run manually:
 
-#tar_make(script = "challenges-solutions/challenge07-solution.R", store = "challenge06")
-# tar_make(script = "challenges-solutions/challenge06-solution.R", store = "challenge06")
-tar_visnetwork(script = "challenge07-solution.R", store = "challenge06")
-#tar_make(script = "challenges-solutions/challenge09-solution.R", store = "challenge09")
-tar_make(script = "challenges-solutions/MASTER-solution-pipeline.R", 
+
+tar_visnetwork(script = "challenges-solutions/challenge08-solution.R", store = "mastersolution")
+
+tar_make(script = "challenge08-solution.R", 
          store = "mastersolution")
 
 
-#tar_make(script = "challenge05-solution.R", store = "challenge05")
-tar_make(script = "challenge06-solution.R", store = "challenge06")
-
-# load targets manually from a manually selected store:
-#tar_load("rm_constants", store = "challenge05")
 
 
-tar_manifest(script = "challenge07-solution.R")
